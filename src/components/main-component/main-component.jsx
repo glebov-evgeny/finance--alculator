089e4d6032ae77454e7f6631f1cfaa5957ca181b
Glebov Evgeny <egoji@mail.ru>
2022-06-27 10:01:57 +0300

    css

--- src/components/main-component/main-component.jsx
+++ src/components/main-component/main-component.jsx
@@ -72,19 +72,21 @@ function MainComponent() {
                     <div className="layer__info">
                         <p className="layer__info-text">Здесь возможно будет поясняющий текст</p>
                     </div>
-                    <div className="layer__logic">
-                        <div className="layer__input-block">
-                            <input type="text" className="layer__input"/>
+                    <div className="layer__bottom-composition">
+                        <div className="layer__logic">
+                            <div className="layer__input-block">
+                                <input type="text" className="layer__input"/>
+                            </div>
+                            <div className="layer__logic-toggle">
+                                <button className="layer__logic-tumbler _in">
+                                    <img src={arrow} className="layer__logic-tumbler-img" alt="arrow" />
+                                </button>
+                            </div>
                         </div>
-                        <div className="layer__logic-toggle">
-                            <button className="layer__logic-tumbler layer__logic-in">
-                                <img src={arrow} alt="arrow" />
-                            </button>
+                        <div className="layer__actions">
+                            <button className="layer__actions-btn _submit">Отправить</button>
+                            <button className="layer__actions-btn _clear">Отменить</button>
                         </div>
-                    </div>
-                    <div className="layer__actions">
-                        <button className="layer__actions-btn _submit">Отправить</button>
-                        <button className="layer__actions-btn _clear">Отменить</button>
                     </div>
                 </div>
             </div>
