fae32e7fc182663f519cd67d1e2b264cb5d9466a
Евгений Глебов <egoji@mail.ru>
2022-06-27 23:23:11 +0300

    fix

--- src/components/main-component/main-component.jsx
+++ src/components/main-component/main-component.jsx
@@ -5,11 +5,14 @@ import arrow from "../../assets/img/common/arrow.svg";
 function MainComponent() {
     const [value, setValue] = useState('');
     const [information, setInformation] = useState([]);
+    const [totalSum, setTotalSumm] = useState(0)
     const db = getFirestore();
 
+    /* отправка данных */
     const sendMessage = async () => {
         await addDoc(collection(db, 'items'), {
             price: Number(value),
+            status: true,
             createAt: serverTimestamp()
         });
         setValue('');
@@ -20,81 +23,81 @@ function MainComponent() {
         const getData = query(collection(db, 'items'))
         onSnapshot(getData, (querySnapshot) => {
             /* перевожу объекты в массив */
-            let informationArr = [];
+            let informationArr = [],
+                result = 0
             querySnapshot.forEach(function(doc) {
                 // console.log(doc.data().createAt, " => ", doc.data());
                 informationArr.push(doc.data());
             })
-            /* добавляю в стэйт */
+            /* добавляю в стэйт итоговую сумму */
+            for (let item of informationArr){
+                result+= item.price
+            }
+            // eslint-disable-next-line
+            setTotalSumm(result)
+
+            /* добавляю в стэйт  */
+            // eslint-disable-next-line
             setInformation(informationArr);
         });
+
     }, [db])
 
+    /* Разбивка числа по разрядам */
+    function getPriceNmb(props) {
+        return props.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')
+    }
+
+    /* Получение даты */
+    function getDate(props) {
+        let result = props.toDate();
+        let dd = result.getDate();
+        if (dd < 10) dd = '0' + dd;
+
+        let mm = result.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+
+        let yy = result.getFullYear();
+
+        return dd + '.' + mm + '.' + yy;
+    }
 
     return (
         <div className="layer">
             <div className="container">
                 <div className="layer__top">
-                    <p className="layer__price"><span className="layer__price-total">123 456</span> &#8381;</p>
+                    <p className="layer__price"><span className="layer__price-total">{getPriceNmb(totalSum)}</span> &#8381;</p>
                     {/*<p>{{information}}</p>*/}
                 </div>
                 <div className="layer__block">
                     <div className="layer__aside">
                         <p className="layer__block-info">Траты:</p>
                         <div className="layer__minus layer__pull">
-                            <div className="layer__card">
-                                <div className="layer__card-content">
-                                    <p className="layer__card-price"><span className="layer__card-price-total">123 456</span> &#8381;</p>
-                                    <div className="layer__card-line"></div>
-                                    <p className="layer__card-date">11.11.2022</p>
+                            {information.map((item) => (
+                                <div className="layer__card" key={item.createAt}>
+                                    <div className="layer__card-content">
+                                        <p className="layer__card-price"><span className="layer__card-price-total">{item.price}</span> &#8381;</p>
+                                        <div className="layer__card-line"></div>
+                                        {/*<p className="layer__card-date">{getDate(item.createAt)}</p>*/}
+                                    </div>
+                                    <button className="layer__card-btn">x</button>
                                 </div>
-                                <button className="layer__card-btn">x</button>
-                            </div>
-                            <div className="layer__card">
-                                <div className="layer__card-content">
-                                    <p className="layer__card-price"><span className="layer__card-price-total">123 456</span> &#8381;</p>
-                                    <div className="layer__card-line"></div>
-                                    <p className="layer__card-date">11.11.2022</p>
-                                </div>
-                                <button className="layer__card-btn">x</button>
-                            </div>
+                            ))}
                         </div>
                     </div>
                     <div className="layer__main">
                         <p className="layer__block-info">Поступления:</p>
                         <div className="layer__plus layer__pull">
-                            <div className="layer__card">
-                                <div className="layer__card-content">
-                                    <p className="layer__card-price"><span className="layer__card-price-total">123 456</span> &#8381;</p>
-                                    <div className="layer__card-line"></div>
-                                    <p className="layer__card-date">11.11.2022</p>
+                            {information.map((item) => (
+                                <div className="layer__card" key={item.createAt}>
+                                    <div className="layer__card-content">
+                                        <p className="layer__card-price"><span className="layer__card-price-total">{item.price}</span> &#8381;</p>
+                                        <div className="layer__card-line"></div>
+                                        {/*<p className="layer__card-date">{getDate(item.createAt)}</p>*/}
+                                    </div>
+                                    <button className="layer__card-btn">x</button>
                                 </div>
-                                <button className="layer__card-btn">x</button>
-                            </div>
-                            <div className="layer__card">
-                                <div className="layer__card-content">
-                                    <p className="layer__card-price"><span className="layer__card-price-total">123 456</span> &#8381;</p>
-                                    <div className="layer__card-line"></div>
-                                    <p className="layer__card-date">11.11.2022</p>
-                                </div>
-                                <button className="layer__card-btn">x</button>
-                            </div>
-                            <div className="layer__card">
-                                <div className="layer__card-content">
-                                    <p className="layer__card-price"><span className="layer__card-price-total">123 456</span> &#8381;</p>
-                                    <div className="layer__card-line"></div>
-                                    <p className="layer__card-date">11.11.2022</p>
-                                </div>
-                                <button className="layer__card-btn">x</button>
-                            </div>
-                            <div className="layer__card">
-                                <div className="layer__card-content">
-                                    <p className="layer__card-price"><span className="layer__card-price-total">123 456</span> &#8381;</p>
-                                    <div className="layer__card-line"></div>
-                                    <p className="layer__card-date">11.11.2022</p>
-                                </div>
-                                <button className="layer__card-btn">x</button>
-                            </div>
+                            ))}
                         </div>
                     </div>
                 </div>
