45537de7a665805437066927371e7ce6822f7885
Glebov Evgeny <egoji@mail.ru>
2022-06-27 18:01:59 +0300

    test

--- src/components/main-component/main-component.jsx
+++ src/components/main-component/main-component.jsx
@@ -1,13 +1,42 @@
-import arrow from "../../assets/img/common/arrow.svg"
-// import { getFirestore, collection, addDoc, serverTimestamp, onSnapshot, query } from "firebase/firestore"
+import { useState, useEffect} from "react";
+import { getFirestore, collection, addDoc, serverTimestamp, onSnapshot, query } from "firebase/firestore"
+import arrow from "../../assets/img/common/arrow.svg";
 
 function MainComponent() {
+    const [value, setValue] = useState('');
+    const [information, setInformation] = useState([]);
+    const db = getFirestore();
+
+    const sendMessage = async () => {
+        await addDoc(collection(db, 'items'), {
+            price: Number(value),
+            createAt: serverTimestamp()
+        });
+        setValue('');
+    };
+
+    /* Получение данных */
+    useEffect(() => {
+        const getData = query(collection(db, 'items'))
+        onSnapshot(getData, (querySnapshot) => {
+            /* перевожу объекты в массив */
+            let informationArr = [];
+            querySnapshot.forEach(function(doc) {
+                // console.log(doc.data().createAt, " => ", doc.data());
+                informationArr.push(doc.data());
+            })
+            /* добавляю в стэйт */
+            setInformation(informationArr);
+        });
+    }, [db])
+
 
     return (
         <div className="layer">
             <div className="container">
                 <div className="layer__top">
                     <p className="layer__price"><span className="layer__price-total">123 456</span> &#8381;</p>
+                    {/*<p>{{information}}</p>*/}
                 </div>
                 <div className="layer__block">
                     <div className="layer__aside">
@@ -76,7 +105,12 @@ function MainComponent() {
                     <div className="layer__bottom-composition">
                         <div className="layer__logic">
                             <div className="layer__input-block">
-                                <input type="text" className="layer__input"/>
+                                <input
+                                    type="number"
+                                    className="layer__input"
+                                    value={value}
+                                    onChange={event => setValue(event.target.value)}
+                                />
                             </div>
                             <div className="layer__logic-toggle">
                                 <button className="layer__logic-tumbler _in">
@@ -85,7 +119,7 @@ function MainComponent() {
                             </div>
                         </div>
                         <div className="layer__actions">
-                            <button className="layer__actions-btn _submit">Отправить</button>
+                            <button className="layer__actions-btn _submit" onClick={sendMessage}>Отправить</button>
                             <button className="layer__actions-btn _clear">Отменить</button>
                         </div>
                     </div>
